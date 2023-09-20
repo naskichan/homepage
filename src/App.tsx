d25c3f26ae4bb55576dc8c5d9303a0ef1444343c
Naskichan <gitlab@naski.org>
2023-09-20 12:15:46 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className="font-white">This is a heading</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'background': '#23292E',
+        'accent': '#373f47',
+        'light': '#F6F4F5',
+      },
+    },
+  },
+  plugins: [],
+};
